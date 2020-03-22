cc7369c1a8adde033c652b3fa80894bd56c0cbf2
BaldiJr <junior.baldi@hotmail.com>
2020-03-22 13:23:21 -0300

    first commit

---
+++ GameTOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GameTOP.Lib;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var jogo1 = new JogoFODA(
+               new Jogador1("Ronaldo"), 
+               new Jogador2("Zidane"));
+           jogo1.IniciarJogo();
+        }       
+
+    }
+}
